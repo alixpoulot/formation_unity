1b89e0ef6f346c67b4a951d35974e7c39c45f025
Alix Poulot <alix.poulot@gmail.com>
2015-12-03 11:35:37 +0100

    Scripts button credits description and Load Scene MainGame

---
+++ GoeSciences3d/Assets/Scripts/SetButtonAlpha.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetButtonAlpha : MonoBehaviour
+{
+
+    public void SetAlpha(float alpha)
+    {
+        Color newColor = GetComponent<UnityEngine.UI.Image>().color;
+        newColor.a = alpha;
+        GetComponent<UnityEngine.UI.Image>().color = newColor;
+    }
+}
